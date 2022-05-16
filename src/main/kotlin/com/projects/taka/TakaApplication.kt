50c4958b2f8e13273034a5028f676bd1f3234973
josephgchdid <josefgchdid@gmail.com>
2022-05-16 22:50:29 +0300

    Initial commit

---
+++ src/main/kotlin/com/projects/taka/TakaApplication.kt
@@ -0,0 +1,11 @@
+package com.projects.taka
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TakaApplication
+
+fun main(args: Array<String>) {
+	runApplication<TakaApplication>(*args)
+}
---
+++ src/test/kotlin/com/projects/taka/TakaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.projects.taka
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TakaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
